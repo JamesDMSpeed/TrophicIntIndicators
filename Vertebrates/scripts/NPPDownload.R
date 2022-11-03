5d7a1f1327398bf2cb2465594e1b29dd6896f62d
JamesDMSpeed <james.speed@ntnu.no>
2022-11-03 15:53:20 +0100

    NPP data

---
+++ Vertebrates/scripts/NPPDownload.R
@@ -0,0 +1,42 @@
+#NPP
+
+#Downloading MODIS NPP data
+
+library(MODIStsp)
+library(terra)
+library(sf)
+library(tidyverse)
+
+
+#Using MODIStsp package
+
+#List product names
+MODIStsp_get_prodnames()
+
+#List band names within the NPP product
+MODIStsp_get_prodlayers("M*D17A3HGF")$bandnames
+
+
+#Download 
+#Set bounding box (xmin, ymin, xmax,ymax), dates, folder, band and sensor and output format
+
+MODIStsp(gui             = FALSE,
+         out_folder      = 'Vertebrates/data/NPP',
+         out_folder_mod  = 'Vertebrates/data/NPP',
+         selprod         = "Net_PP_GapFil_Yearly_500m (M*D17A3HGF)", 
+         bandsel         = 'Npp', 
+         sensor          = 'Terra',
+         user            = 'james.speed_1' , # your username for NASA http server
+         password        = 'ModisPass1',  # your password for NASA http server
+         start_date      = '2000.01.01', 
+         end_date        = '2022.12.31', 
+         verbose         = TRUE,
+         bbox            =  c(-76208 ,6450245 , 1114929 , 7939986), #bbox of Norway
+         spatmeth        = 'bbox',
+         out_format      = 'GTiff',
+         compress        = 'LZW',
+         out_projsel     = 'User Defined',
+         output_proj     = '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
+         delete_hdf      = TRUE,
+         parallel        = TRUE
+)
